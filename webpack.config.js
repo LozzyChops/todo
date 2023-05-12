3be587a0de9b825c9bf521950536f0670b975463
LozzyChops <lephilim@gmail.com>
2023-05-12 13:48:52 -0500

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  plugins: [new HtmlWebpackPlugin()],
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-};
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ],
+    mode: 'development',
+    output: {
+        clean: true
+    },
+    devServer: {
+        static: './dist',
+        open: true
+    }
+}
