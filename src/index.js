96b8abdf8edc2c1257b24cd46ee9f58d395da16e
LozzyChops <lephilim@gmail.com>
2023-06-01 11:31:43 -0500

    Fix presence of debugging code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-import './styles.css';
-import { userInterface } from "./UI";
-import { Project, Task, projects, projectControls } from "./tools";
-import { DOMNodes, setUpInitHTML, displayProjectName, displayTask, swapSelected, emptyProjectsDisplay, emptyTasksDisplay } from './DOM'
+import './styles.css'
+import { userInterface } from './UI'
 
-userInterface.displayHomePage();
-
-//TEMP - debugging additions
-/*
-window.userInterface = userInterface
-window.Project = Project    
-window.Task = Task
-window.projectControls = projectControls
-window.projects = projects
-window.DOMNodes = DOMNodes
-window.setUpInitHTML = setUpInitHTML
-window.displayProjectName = displayProjectName
-window.displayTask = displayTask
-window.swapSelected = swapSelected
-window.emptyProjectsDisplay = emptyProjectsDisplay
-window.emptyTasksDisplay = emptyTasksDisplay
-*/
+userInterface.displayHomePage()
